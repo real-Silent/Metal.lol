a2e9cc19184d07ed9ceef27fcf070f3ec0ed819e
Silent <idk7778001@gmail.com>
2024-10-24 01:08:45 +0100

    PluginInfo.cs

---
+++ PluginInfo.cs
@@ -0,0 +1,10 @@
+namespace StupidTemplate
+{
+    internal class PluginInfo
+    {
+        public const string GUID = "org.silent.gorillatag.MetalLol";
+        public const string Name = "Metal";
+        public const string Description = "Created by Silent and Asteroid | (@s1lnt || @asteroidrealaccount) on discord";
+        public const string Version = "1.0.0";
+    }
+}
